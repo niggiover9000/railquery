666b49201e29827c6aa61fccdd88aa5b8aa3667b
niggiover9000 <nicolasbartels@web.de>
2025-05-21 18:11:57 +0200

    Added description

--- static/js/custom.js
+++ static/js/custom.js
@@ -1,2 +1,3 @@
+// This enables support for some bootstrap functions that are used on the page
 const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
 const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
