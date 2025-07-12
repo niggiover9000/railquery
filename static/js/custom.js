64d45c350330d17a3ee05641fa48d5919dd073ad
niggiover9000 <nicolasbartels@web.de>
2025-07-12 17:51:46 +0200

    Hotfix for tooltips

--- static/js/custom.js
+++ static/js/custom.js
@@ -1,3 +1,4 @@
-// This enables support for some bootstrap functions that are used on the page
-const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
-const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
+document.addEventListener('DOMContentLoaded', function () {
+        const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]');
+        const tooltipList = [...tooltipTriggerList].map(el => new bootstrap.Tooltip(el));
+    });
